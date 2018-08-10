95473586711adcbb55b130f067dfa01559d55027
DemonSkye <damien.bell.27@gmail.com>
2018-08-10 13:26:23 -0400

    Lesson 5 source code.

--- src/Lesson5.java
+++ src/Lesson5.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Lesson5 {
     public static void main(String args[]){
         //Hard typing
@@ -13,5 +15,33 @@ public class Lesson5 {
         //1. You're not pushing code that you're not sure of / don't know what it does, into production
         //2. If you do know what type you need, use that type
         //3. It's a nightmare to figure out what someone planned to use that variable for when you soft type.
+
+        // ==================================================================================================
+
+        //How to take input from the user
+        Scanner scan = new Scanner(System.in);
+
+        //System.out.println("Enter a number: ");
+        //int a = scan.nextInt();
+
+        //System.out.println("You entered: " + a);
+
+        //For double use
+        //double b = scan.nextDouble();
+
+        System.out.println("Enter as many words as you want followed by the <Enter> key : ");
+        String str = scan.nextLine();
+
+        System.out.println(str);
+
+        // ==================================================================================================
+        //Suggested homework:
+        //Create a program that allows a user to input some numbers, representing a bank account.
+        //Once the user has entered their current balance, and their rate of interest, tell them how much money they will have in 1 year, 2 years, 5 years and 10 years.
+        //Compounding interest (remeber Math.pow() that we looked at earlier.
+        //or Simple interest
+
+
+
     }
 }
